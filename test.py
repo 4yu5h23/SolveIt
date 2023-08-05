9d04f5a6a292899f6d225b34bb7b8a787cfe587d
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2023-08-05 15:13:25 +0530

    added documentation and renamed mod to Matrices

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-import mod
+import Matrices
+M=[[4,0,0],[0,4,0],[0,0,2]]
 
-
-print(mod.eigenvals([[4,0,0],[0,4,0],[0,0,2]]))
+print(Matrices.traceofmatrix(M))
+print(Matrices.sumofminors(M))
+print(Matrices.determinantofmatrix(M))
+print(Matrices.characteristiceqn(M))
+print(Matrices.eigenvals(M))
